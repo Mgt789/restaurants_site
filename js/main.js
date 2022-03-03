11fd0a644c44c705a09351bea8747f18fe1557e3
Chandan Meher <cmeher20@gmail.com>
2022-03-03 15:23:37 +0530

    Add project files.

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// navbar
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+function offNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+// active-item
+// var header = document.getElementById("myDIV");
+// var btns = header.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName("active");
+//         if (current.length > 0) {
+//             current[0].className = current[0].className.replace(" active", "");
+//         }
+//         this.className += " active";
+//     });
+// }
+
+
+
+// nav hide
+
+// counter design
